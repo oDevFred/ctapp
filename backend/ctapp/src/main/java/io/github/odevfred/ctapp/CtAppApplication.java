62311af52c88de7c563372f76aeb172204d2a0dd
oDevFred <caio.frederico2001@outlook.com>
2026-03-10 22:54:34 -0300

    first commit

---
+++ backend/ctapp/src/main/java/io/github/odevfred/ctapp/CtAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.odevfred.ctapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtAppApplication.class, args);
+	}
+
+}
---
+++ backend/ctapp/src/test/java/io/github/odevfred/ctapp/CtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.odevfred.ctapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
